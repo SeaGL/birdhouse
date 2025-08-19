e6494b800a717d8e20d8b325ab82efc7c6077064
AJ Jordan <alex@strugee.net>
2025-08-18 23:43:27 -0400

    Add Centrifugo boilerplate to client JS

--- main.js
+++ main.js
@@ -20,17 +20,17 @@ function fetchStreamDataLive() {
 
 function fetchStreamDataTest() {
 	// 2024's day 2 data, in case you're wondering
-	var testData = [{"name":"Lyceum","youtube_id":"uI7gJ8WtQ-I"},{"name":"Room 332","youtube_id":"1lXZnncGgjc"},{"name":"Room 334","youtube_id":"sYKOpPgzWPE"},{"name":"Room 340","youtube_id":"wJfbS8f2Zio"}];
+	var testData = [{"name":"Lyceum","youtube_id":"uI7gJ8WtQ-I"},{"name":"Virtual Test Room","youtube_id":"1lXZnncGgjc"},{"name":"Room 334","youtube_id":"sYKOpPgzWPE"},{"name":"Room 340","youtube_id":"wJfbS8f2Zio"}];
 
 	// Delay by a quarter of a second to simulate network load
 	return new Promise(resolve => setTimeout(resolve.bind(null, testData), 250));
 }
 
-//
-// YOUTUBE STREAM MONITORING
-//
-
 (async function() {
+	//
+	// YOUTUBE STREAM MONITORING
+	//
+
 	var data = await (loadTestData ? fetchStreamDataTest : fetchStreamDataLive)();
 
 	var ytMonitorEl = document.getElementById('yt-monitor-tab-pane-inner');
@@ -41,12 +41,45 @@ function fetchStreamDataTest() {
 			<iframe class="embed-responsive-item" src="https://www.youtube-nocookie.com/embed/${ streamData.youtube_id }?autoplay=0&mute=1&cc_load_policy=1&cc_lang_pref=en&modestbranding=1&rel=0" allow="encrypted-media; picture-in-picture" allowfullscreen></iframe>
 			</div>`;
 	}).reduce((a, b) => a + b);
-})();
 
-//
-// STREAM CONTROL
-//
+	//
+	// CENTRIFUGO SETUP
+	//
 
-(function() {
-	// TODO	
+	var centrifuge = new Centrifuge('ws://localhost:8000/connection/websocket', {
+		token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM3MjIiLCJpYXQiOjE3NTU1NzM0MjF9._X4rZbCi8LyWNaTtOBlrQzACAyiZgOU3abPoGgldoe4'
+	});
+
+	centrifuge.on('connecting', function(ctx) {
+		console.log(`Connecting to Centrifugo: ${ctx.code}, ${ctx.reason}`);
+	}).on('connected', function(ctx) {
+		console.log(`Connected to Centrifugo over transport ${ctx.transport}`);
+	}).on('disconnected', function(ctx) {
+		var errmsg = `Disconnected from Centrifugo, code ${ctx.code}, reason ${ctx.reason}`;
+		console.error(errmsg);
+		alert(errmsg);
+	}).connect();
+
+	var channelSubs = {};
+
+	var channelList = data.map(obj => 'host_control_' + obj.name.replaceAll(' ', '_').toLowerCase());
+	channelList.push('obs_heartbeats');
+
+	channelList.forEach(function(channelName) {
+		var sub = centrifuge.newSubscription(channelName);
+		channelSubs[channelName] = sub;
+
+		sub.on('publication', function(ctx) {
+			console.log(`Received Centrifugo publication from \`${sub.channel}\`:`);
+			console.dir(ctx.data);
+		}).on('subscribing', function(ctx) {
+			console.log(`Subscribing to Centrifugo channel \`${sub.channel}\`, code ${ctx.code}, reason ${ctx.reason}`);
+		}).on('subscribed', function(ctx) {
+			console.log(`Subscribed to Centrifugo channel \`${sub.channel}\`, context:`, ctx);
+		}).on('unsubscribed', function(ctx) {
+			var errmsg = `Unsubscribed from Centrifugo channel \`${sub.channel}\`, code ${ctx.code}, reason ${ctx.reason}`;
+			console.error(errmsg);
+			alert(errmsg);
+		}).subscribe();
+	});
 })();
