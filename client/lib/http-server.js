543d984f847b3a486499db202c8646265f7e5c70
AJ Jordan <alex@strugee.net>
2025-10-19 19:11:18 -0400

    Use Express compression()

--- client/lib/http-server.js
+++ client/lib/http-server.js
@@ -1,12 +1,16 @@
 'use strict';
 
 var express = require('express');
+var compression = require('compression');
+
 var aviaryuiRouter = require('./http-aviaryui');
 
 var app = express();
 var PORT = process.env.PORT || 9000;
 
 function startServer() {
+	app.use(compression());
+
 	app.use('/aviaryui', aviaryuiRouter);
 
 	app.listen(PORT, () => {
