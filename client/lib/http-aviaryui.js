c3395e9d6e086ad8d442ecd7df7697385f35ef91
AJ Jordan <alex@strugee.net>
2025-10-19 21:44:04 -0400

    Hook up aviaryui control route to OBS

--- client/lib/http-aviaryui.js
+++ client/lib/http-aviaryui.js
@@ -2,12 +2,21 @@
 
 var express = require('express');
 
+var obs = require('./obs');
+
 var router = express.Router();
 
-router.get('/set-livestream-state', (req, res) => {
+router.get('/set-livestream-state', async function(req, res) {
 	var state = JSON.parse(req.query.state.toLowerCase());
 	console.log('Received request to set livestream state: ' + state);
-	// TODO
+
+	// TODO update these with the actual names
+	var targetScene = state ? 'Room' : 'Ad Roll';
+
+	var client = await obs();
+	await client.call('SetCurrentProgramScene', {sceneName: targetScene});
+
+	// TODO lol should we like... actually send something back
 	res.send();
 });
 
